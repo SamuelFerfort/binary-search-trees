8d56775b9bbfb12ab773d9d70a0977320a2bc79b
Samuel <samuferfort@gmail.com>
2024-05-10 04:31:50 +0200

    add basic classes

---
+++ bst.js
@@ -0,0 +1,20 @@
+class Node {
+    constructor(data) {
+        this.data = data
+        this.left = null
+        this.right = null
+    }
+}
+
+class Tree {
+    constructor(array){
+        this.root = buildTree(array)
+    }
+}
+
+
+
+function buildTree(array) {
+    // should return level-0 root node
+    
+}
